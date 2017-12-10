9270639e1977a8e6cb498803a2bc7f711a473d8b
Thomas Pucci <thomas.lb.pucci@gmail.com>
2017-12-10 19:09:45 +0100

    feat(app): Setup app

--- app/src/App.js
+++ app/src/App.js
@@ -16,7 +16,7 @@ export default class App extends Component<{}> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.welcome}>Welcome to Hey Pucci!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
