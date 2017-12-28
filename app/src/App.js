ebcefa2a2607810a24a3cfd41eb2d25d86bc2c60
Thomas Pucci <thomas.lb.pucci@gmail.com>
2017-12-28 18:13:47 +0100

    build(app): Base app for the dojo

--- app/src/App.js
+++ app/src/App.js
@@ -1,61 +1,29 @@
 // @flow
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
-import { toIdValue } from 'apollo-utilities';
-import { ApolloProvider } from 'react-apollo';
-import { ApolloClient } from 'apollo-client';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { HttpLink } from 'apollo-link-http';
 
 import TodosList from './components/TodosList';
 
-const cache = new InMemoryCache({
-  dataIdFromObject: o => o.id,
-  cacheResolvers: {
-    /*Query: {
-      Todo: (_, args) =>
-        toIdValue(
-          cache.config.dataIdFromObject({
-            __typename: 'Todo',
-            id: args.id,
-          })
-        ),
-    },*/
-  },
-});
-
-const link = new HttpLink({
-  uri:
-    'http://localhost:60000/simple/v1/cjazndqsm000b01395d7v8hwh',
-});
-
-const client = new ApolloClient({
-  link,
-  cache,
-});
-
 function App(props: PropsType) {
   return (
-    <ApolloProvider client={client}>
-      <View style={styles.wrapper}>
-        <TodosList
-          todos={[
-            {
-              id: 1,
-              text: 'Task A',
-            },
-            {
-              id: 2,
-              text: 'Task B',
-            },
-            {
-              id: 3,
-              text: 'Task C',
-            },
-          ]}
-        />
-      </View>
-    </ApolloProvider>
+    <View style={styles.wrapper}>
+      <TodosList
+        todos={[
+          {
+            id: 1,
+            text: 'Task A',
+          },
+          {
+            id: 2,
+            text: 'Task B',
+          },
+          {
+            id: 3,
+            text: 'Task C',
+          },
+        ]}
+      />
+    </View>
   );
 }
 
